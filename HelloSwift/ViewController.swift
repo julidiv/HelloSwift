fba44249d41bf27cabd0442eef1e3f8ad1c1f61e
alexandergolub <alexander.golub@digitallyinspired.com>
2022-06-19 17:43:29 +0200

    1.setup UI 2.Work with logic

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var grettingLabel: UILabel!
+    
+    @IBOutlet var grettingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        grettingLabel.isHidden = true
+        grettingButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func grettingButtumTapped() {
+        grettingLabel.isHidden.toggle()
+        grettingButton.setTitle(grettingLabel.isHidden ? "Show gretting" : "Hide gretting", for: .normal)
+    }
 }
 
